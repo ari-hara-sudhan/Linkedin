955b92550f2672a34a217824db8afc6fca9c7ce0
=ari <=ahshari2003@gmail.com>
2021-07-09 16:32:40 +0530

    auth part is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './Header'
 import Sidebar from './Sidebar'
 import "./App.css"
 import Feed from "./Feed"
-import { useSelector } from 'react-redux'
-import { selectUser } from './features/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/userSlice'
 import Login from "./Login"
+import { auth } from './firebase'
 function App() {
   const user =useSelector(selectUser)
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    auth.onAuthStateChanged(userAuth=>{
+      if(userAuth){
+        dispatch(login({
+          email:userAuth.email,
+          uid:userAuth.uid,
+          displayName:userAuth.displayName,
+          photo:userAuth.photoURL,
+
+        }))
+        
+      }else{
+        dispatch(logout())
+      }
+
+    })
+ 
+
+  },[])
   return (
     <div className="app">
       <Header/>
--- src/Login.js
+++ src/Login.js
@@ -2,31 +2,78 @@ import React, { useState } from 'react'
 import "./Login.css"
 import { Button } from '@material-ui/core'
 import { auth, provider } from './firebase'
+import { useDispatch } from 'react-redux'
+import { login } from './features/userSlice'
 function Login() {
-    const[email,setEmail]=useState()
-    const[password,setPassword]=useState()
-    const[name,setName]=useState()
-    const[profile,setProfile]=useState()
+    const[email,setEmail]=useState("")
+    const[password,setPassword]=useState("")
+    const[name,setName]=useState("")
+    const[profile,setProfile]=useState("")
+    const dispatch=useDispatch()
     const signin=(e)=>{
       e.preventDefault();
-      auth.signInWithPopup(provider)
-      .catch((error)=>alert(error.message))
+      
     }
+    
     const register=()=>{
+        if(!name){
+            return alert ("please enter the full name")
+        }
+        auth.createUserWithEmailAndPassword(email,password)
+        .then((userAuth)=>{
+            userAuth.user.updateProfile({
+                displayName:name,
+                photoURL:profile,
+               
 
+            })
+            .then(()=>{
+               dispatch(login({
+                   email:userAuth.user.email,
+                   uid:userAuth.user.uid,
+                   displayName:name,
+                   photoURL:profile,
+               })) 
+            }).catch(error=>alert(error.message))
+        })
+
+        setPassword("")
+        setEmail("")
+        setName("")
+        setProfile("")
     }
+    
     return (
         <div className="login">
            <div className="login__logo">
-               <img src="https://www.bing.com/th?id=OIP.Ff1a2zx1DnGg5rppyqq-XwHaHa&w=105&h=105&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2"/>
+               <img src="https://www.bing.com/th?id=OIP.Ff1a2zx1DnGg5rppyqq-XwHaHa&w=105&h=105&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2" alt="image"/>
            </div>
             <form>
-                <input value={name} onChange={e=>setName(e.target.value)} placeholder="Full name is {required}"
-                type="text"
+                <input
+                placeholder="Enter the full name"
+                value={name}
+                type='text'
+                onChange={e=>setName(e.target.value)}
+                 />
+                <input 
+                placeholder="enter the profile "
+                value={profile}
+                onChange={e=>setProfile(e.target.value)}
                 />
-                <input value={profile} onChange={e=>e.target.value} type="text" placeholder="Profile image url(optional)"/>
-                <input value={email} onChange={e=>setEmail(e.target.value)} type="text" placeholder="Enter the email"/>
-                <input value={password} onChange={e=>setPassword(e.target.value)} type="password" placeholder="Enter the password"/>
+                <input
+                placeholder="enter the email"
+                type="email"
+                value={email}
+                onChange={e=>setEmail(e.target.value)}
+                 />
+                <input 
+                placeholder="enter the password"
+                type="password"
+                value={password}
+                onChange={e=>setPassword(e.target.value)}
+                />
+              
+              
             <Button className="login__button" onClick={signin}>Sign In</Button>
             </form>
             <p>Not a memeber?<span className="login__register" onClick={register}>Register Now</span></p>
